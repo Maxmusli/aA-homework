6b4ea3939a88376ba2a610747614545a3a061692
max <maxmus2223@gmail.com>
2019-09-19 19:29:36 -0700

    test

---
+++ W5D5/House/app/models/house.rb
@@ -0,0 +1,3 @@
+class House < ApplicationRecord
+  validates :
+end
---
+++ W5D5/House/app/models/person.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id       :bigint           not null, primary key
+#  name     :string           not null
+#  house_id :integer
+#
+
+class Person < ApplicationRecord
+  validates :name, presence: true
+  
+end
