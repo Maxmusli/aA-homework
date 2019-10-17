0f8f2692ca7367be9ab6c5f33016482d6e9062eb
max <maxmus2223@gmail.com>
2019-10-16 21:34:21 -0700

    create JQuary exercises on JS

--- W8D5/practice.js
+++ W8D5/practice.js
@@ -1,19 +1,19 @@
-const readline = require('readline');
+// const readline = require('readline');
 
-const reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const reader = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
-reader.question("What is your name?", function (answer) {
-  console.log(`Hello ${answer}!`);
+// reader.question("What is your name?", function (answer) {
+//   console.log(`Hello ${answer}!`);
 
-  // Close the reader, which will allow the program to end because it
-  // is no longer waiting for any events.
-  reader.close();
-});
+//   // Close the reader, which will allow the program to end because it
+//   // is no longer waiting for any events.
+//   reader.close();
+// });
 
-console.log("Last program line");
+// console.log("Last program line");
 
 // ----------------------------------
 
